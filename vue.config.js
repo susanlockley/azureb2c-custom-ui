c84a961813e0f72d681d5d95dbc8a5aa59133764
susanlockley <susanlockley@hotmail.com>
2024-05-13 17:25:43 +1000

    test

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   //  publicPath: 'https://icy-coast-0ec2d7300.5.azurestaticapps.net/'
-    configureWebpack: {
-    output: {
-      publicPath: 'https://icy-coast-0ec2d7300.5.azurestaticapps.net/'
-    }
-  }
+    // configureWebpack: {
+    // output: {
+    //   publicPath: 'https://icy-coast-0ec2d7300.5.azurestaticapps.net/'
+    // }
+  //}
 })
