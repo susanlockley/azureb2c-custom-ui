15cc69bf12fb463b39a5d562d046d8b8c9183f48
susanlockley <susanlockley@hotmail.com>
2024-05-13 15:57:33 +1000

    trial for routing on azure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <HeaderComponent/>
-  <!-- <nav> -->
+  <nav>
     <!-- <router-link to="/">Home</router-link> | -->
-    <!-- <router-link to="/about">About</router-link> -->
-  <!-- </nav> -->
+    <router-link to="/about">About</router-link>
+  </nav>
   <router-view/>
   <!-- <signupForm/> -->
   <FooterComponent/>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: 'https://icy-coast-0ec2d7300.5.azurestaticapps.net/'
 })
