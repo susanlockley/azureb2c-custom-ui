b66433498d2af311b4bfae38f43b21ebc9840251
susanlockley <susanlockley@hotmail.com>
2024-05-15 08:46:01 +1000

    dd

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-    // publicPath: 'https://icy-coast-0ec2d7300.5.azurestaticapps.net/' 
+     publicPath: 'https://icy-coast-0ec2d7300.5.azurestaticapps.net/' 
     //works in b2c for index.html but is applied to b2c url. Does not work for forgottenpassword (no vue route loaduri work)
     //publicPath: './'
     // configureWebpack: {
