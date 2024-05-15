60a76036f4746066cb6a42bbc7ad9a40860d3734
susanlockley <susanlockley@hotmail.com>
2024-05-15 16:28:00 +1000

    dfg

--- public/dfes.js
+++ public/dfes.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     var elemDiv = document.createElement('div');
     elemDiv.id = 'app';
     document.body.appendChild(elemDiv);
+
+    var app = new Vue({
+      el: '#app'
+    })
   })
